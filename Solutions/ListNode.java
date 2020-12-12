31ca2e9739a2f3f54dfdf0c396926ea14a345964
garima-birdi <garimabirdi@gmail.com>
2020-12-12 23:46:07 +0530

    Update ListNode.java
    added iterative method

--- Solutions/ListNode.java
+++ Solutions/ListNode.java
@@ -24,11 +24,12 @@ class Solution {
 		else if (l2 == null)
 			return l1;
 		else {
-			return add(l1, l2, 0);
+			return add(l1, l2, 0); // add(l1,l2);
 		}
 
 	}
 
+	// recursive
 	private ListNode add(ListNode l1, ListNode l2, int carry) {
 		if (l1 == null && l2 == null) {
 			if (carry == 0)
@@ -62,5 +63,51 @@ class Solution {
 
 			return result;
 		}
+	}
+
+	// iterative
+	private ListNode add(ListNode l1, ListNode l2) {
+		
+		int sum = 0, carry = 0;
+		int val1 = 0, val2 = 0;
+		ListNode result = new ListNode();
+		ListNode temp = result;
+
+		while (l1 != null || l2 != null) {
+
+			if (l1 != null) {
+				val1 = l1.val;
+				l1 = l1.next;
+			}
+
+			if (l2 != null) {
+				val2 = l2.val;
+				l2 = l2.next;
+			}
+
+			sum = val1 + val2 + carry;
+			carry = 0;
+			val1 = 0;
+			val2 = 0;
+
+			if (sum > 9) {
+				sum = sum % 10;
+				carry = 1;
+			}
+
+			if (temp == null) {
+				temp = new ListNode(sum, null);
+				result = temp;
+			} else {
+				temp.next = new ListNode(sum, null);
+				temp = temp.next;
+			}
+		}
+
+		if (carry != 0) {
+			temp.next = new ListNode(carry, null);
+		}
+
+		return result;
 	}
 }
