885825c53fc9bd8c50c55350585853daaff636d7
adip-45 <adityaprakash5642@gmail.com>
2020-12-14 00:16:45 +0530

    Update StringReversal.java

--- RecurrsionPractice/src/StringReversal.java
+++ RecurrsionPractice/src/StringReversal.java
@@ -6,7 +6,11 @@ public class StringReversal {
 		if ((str == null) || (str.length() <= 1)) {
 			System.out.println(str);
 		} else {
-			System.out.println(str.charAt(str.length() - 1));
+			if ((str.charAt(str.length() - 1) > 64 && str.charAt(str.length() - 1) < 91)
+					|| (str.charAt(str.length() - 1) > 96 && str.charAt(str.length() - 1) < 123)
+					|| (str.charAt(str.length() - 1) == 32)) {
+				System.out.print(str.charAt(str.length() - 1));
+			}
 			stringReversal(str.substring(0, str.length() - 1));
 		}
 	}
